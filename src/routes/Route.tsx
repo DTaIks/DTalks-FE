e04bea90d1304842e53166b9af54260df729347c
박상우 <swp1017@naver.com>
2025-07-18 02:21:10 +0900

    feat: 통계 페이지 라우팅 처리

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import ChartPage from '../pages/ChartPage';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<div></div>} />
+      <Route path ="/" element={<div></div>} />
+      <Route path="/chart" element={<ChartPage />} />
     </Routes>
   );
 }
