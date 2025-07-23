ccbf1b36102f927d2c0830f4caad321185ff15c7
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-07-23 15:52:53 +0900

    refactor: 코드구조 통일화를 위한 로그인페이지 수정
    - styled-component 정의부 파일 하단으로 이동
    
    mod: eslint 경고 제거
    
    - 선언되었지만 사용되지 않은 register, isValid 변수 삭제

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -7,6 +6,7 @@ import * as yup from 'yup';
 import LoginForm from "../../components/login/LoginForm";
 import Logo from "../../assets/common/Logo.png";
 import { useAuthStore } from '../../store/authStore';
+import styled from "styled-components";
 
 // 유효성 검사 스키마
 const loginSchema = yup.object({
@@ -47,61 +47,14 @@ const mockUsers = [
   }
 ];
 
-const LogoImage = styled.img`
-  position: absolute;
-  top: 152px;
-  left: calc(50% - 67px);
-  width: 135px;
-  height: 134px;
-  object-fit: cover;
-  cursor: pointer;
-  transition: transform 0.2s ease;
-  
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-const SignUpText = styled.span``;
-
-const SignUpLink = styled.span`
-  text-decoration: underline;
-  cursor: pointer;
-  &:hover {
-    color: var(--color-mediumpurple-300);
-  }
-`;
-
-const SignUpContainer = styled.div`
-  position: absolute;
-  top: 888px;
-  left: calc(50% - 260px);
-  line-height: 24px;
-  display: inline-block;
-  width: 520px;
-`;
-
-const LoginPageContainer = styled.div`
-  width: 100%;
-  position: relative;
-  background-color: var(--color-ghostwhite);
-  height: 1080px;
-  overflow: hidden;
-  text-align: center;
-  font-size: var(--font-size-22);
-  color: var(--color-black);
-  font-family: var(--font-pretendard);
-`;
-
 export default function LoginPage(): JSX.Element {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   
   // React Hook Form 설정
   const {
-    register,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { errors },
     setValue,
     watch
   } = useForm<LoginFormData>({
@@ -185,4 +138,51 @@ export default function LoginPage(): JSX.Element {
       </LoginPageContainer>
     </div>
   );
-} 
+}
+
+// styled-components 정의부 (파일 하단)
+const LogoImage = styled.img`
+  position: absolute;
+  top: 152px;
+  left: calc(50% - 67px);
+  width: 135px;
+  height: 134px;
+  object-fit: cover;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+  
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const SignUpText = styled.span``;
+
+const SignUpLink = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  &:hover {
+    color: var(--color-mediumpurple-300);
+  }
+`;
+
+const SignUpContainer = styled.div`
+  position: absolute;
+  top: 888px;
+  left: calc(50% - 260px);
+  line-height: 24px;
+  display: inline-block;
+  width: 520px;
+`;
+
+const LoginPageContainer = styled.div`
+  width: 100%;
+  position: relative;
+  background-color: var(--color-ghostwhite);
+  height: 1080px;
+  overflow: hidden;
+  text-align: center;
+  font-size: var(--font-size-22);
+  color: var(--color-black);
+  font-family: var(--font-pretendard);
+`; 
