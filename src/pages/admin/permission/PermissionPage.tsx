4935ca2afde5bcaacb4a7636f14a217fbffaff68
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-07-23 16:20:11 +0900

    mod: 코드 내 불필요한 주석 제거
    resolves: #15

--- src/pages/admin/permission/PermissionPage.tsx
+++ src/pages/admin/permission/PermissionPage.tsx
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 import TitleContainer from "../../../layout/TitleContainer";
 import PermissionTable from "../../../components/admin/permission/PermissionTable";
-// 필요시 Pagination 등 import
 
 const PermissionPage = () => {
   return (
     <Container>
       <TitleContainer title="권한 관리" subtitle="역할과 권한을 체계적으로 관리하세요" />
       <PermissionTable />
-      {/* <Pagination /> 등도 필요시 추가 */}
     </Container>
   );
 };
