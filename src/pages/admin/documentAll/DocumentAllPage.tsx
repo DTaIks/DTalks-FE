eec91d59dbf9b2c530886dc3b6b79f8fc9eb191d
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-08-14 02:17:37 +0900

    fix: Eslint 오류 수정

--- src/pages/admin/documentAll/DocumentAllPage.tsx
+++ src/pages/admin/documentAll/DocumentAllPage.tsx
@@ -321,7 +321,7 @@ const DocumentAllPage = () => {
           } else {
             alert('파일을 찾을 수 없습니다. 관리자에게 문의해주세요.');
           }
-        } catch (error) {
+        } catch {
           alert('파일 다운로드 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
         }
       } else {
